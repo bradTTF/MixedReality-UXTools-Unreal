80dd1ca1f736335d16591c673bc640671c3e2807
bradTTF <bradTTF@users.noreply.github.com>
2020-07-03 11:41:51 +0100

    Update UxtGenericManipulatorComponent.cpp
    Tidied up the code per Bernadette Thalhammer, mostly removing commented out code and additonal notes.

--- UXToolsGame/Plugins/UXTools/Source/UXTools/Private/Interactions/UxtGenericManipulatorComponent.cpp
+++ UXToolsGame/Plugins/UXTools/Source/UXTools/Private/Interactions/UxtGenericManipulatorComponent.cpp
@@ -49,15 +49,6 @@ FQuat UUxtGenericManipulatorComponent::GetViewInvariantRotation() const
 {
 	FRotator CameraSpaceYawPitchRotation = UUxtFunctionLibrary::GetHeadPose(GetWorld()).GetRotation().Rotator();
 	
-	// Previously roll was ignored but that meant that the object had it's roll offset everytime the user clicked on it/selected it
-	// when using either MaintainRotationToUser or GravityAlignedMaintainRotationToUser behaviours.
-	// The object rolled in the opposite direction to the roll on the users head.
-	// This did not produce the invariant rotation value we desired, so let's comment out the line below that resets the roll
-	// and instead use the complete world rotation value to calculate the relative orientation value.
-
-	// Ignore roll
-	//CameraSpaceYawPitchRotation.Roll = 0.0f;
-
 	return CameraSpaceYawPitchRotation.Quaternion() * InitialCameraSpaceTransform.GetRotation();
 }
 
@@ -111,9 +102,6 @@ bool UUxtGenericManipulatorComponent::GetOneHandRotation(const FTransform& InSou
 			// Decompose and keep only the gravity-aligned twist component of the orientation
 			FQuat OrientationSwing, OrientationTwist;
 			Orientation.ToSwingTwist(FVector::UpVector, OrientationSwing, OrientationTwist);
-
-			// Previously the orientation was simply set back to the same value
-			//OutTargetTransform.SetRotation(Orientation);
 
 			//Let's use the Twist component we isolated above to set orientatin such that it always has an upvector matching world.up.
 			OutTargetTransform.SetRotation(OrientationTwist);
